2a826b7fb32bf78f19b7f721b54b3bc4f54fafee
aayam <aayambishow007@gmail.com>
2021-06-07 10:38:13 +0545

    Add project files.

---
+++ LoggerService/ILoggerManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggerService
+{
+   public interface ILoggerManager
+    {
+        void LogInfo(string Message);
+        void LogWarn(string Message);
+        void LogDebug(string Message);
+        void LogError(string Message);
+    }
+}
